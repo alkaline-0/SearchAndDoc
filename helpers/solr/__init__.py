3ee9410e91db07e255c87105312111f020e24e9b
bhenrich <benjamin@bhenrich.de>
2025-03-16 16:12:43 +0100

    solr new branch

--- helpers/solr/__init__.py
+++ helpers/solr/__init__.py
@@ -1,13 +1,25 @@
 import pysolr
 
-solr = pysolr.Solr('http://localhost:8983/solr/gettingstarted', always_commit=True)
+def create_core(core_name):
+    admin = pysolr.SolrCoreAdmin('http://localhost:8983/solr/admin/cores')
+    resp = admin.create(core_name)
+    print(resp)
 
-document = {
-    'id': '1',
-    'title': 'My first document with local configs',
-}
-solr.add([document])
+def create_document(core, doc_id, doc_title):
+    solr = pysolr.Solr('https://localhost:8983/solr/' + core, always_commit=True)
 
-results = solr.search('title:local')
-for result in results:
-    print(f"ID: {result['id']}, Title: {result['title']}")
+    document = {
+        'id': doc_id,
+        'title': doc_title,
+    }
+
+    solr.add(document)
+
+    results = solr.search('title:local')
+    for result in results:
+        print(f"ID: {result['id']}, Title: {result['title']}")
+
+if __name__ == "__main__":
+    create_core("test")
+
+
